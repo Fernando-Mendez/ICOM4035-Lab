0bd7d989ef60d04678a6cfb9d5a29587d720f2bf
Fernando <fernando@Fresh-Surface.localdomain>
2018-02-19 11:02:56 -0400

    Added the area calculation

---
+++ Main.java
@@ -0,0 +1,16 @@
+//Program which calculates the area of a circle given its radius
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the radius ... ");
+		double radius = scan.nextDouble();
+		
+		System.out.println("The area of the circle is: "+Math.PI*radius*radius);
+		
+	}
+
+}
+
